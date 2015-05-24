120239c7ed530858f94401148b63b13be52667a2
Marcos Bermejo <marcos.bermejo@immfly.com>
2015-05-24 18:44:18 +0200

    mas css

--- js/behaviour.js
+++ js/behaviour.js
@@ -2,3 +2,12 @@ $('.hamburger').click(function(){
    $('.menu-bar').toggleClass('hidden-xs');
    
 });
+
+function resizeSlider(){
+    var height = $(window).height();
+    $('.slider').height(height * 0.75);
+}
+
+$('document').ready(resizeSlider);
+
+$(window).resize(resizeSlider);
